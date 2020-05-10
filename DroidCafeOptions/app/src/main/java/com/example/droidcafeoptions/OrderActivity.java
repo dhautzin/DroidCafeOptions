d6369952019cbd7b9ec83608f460a30b933ece25
dhautzin <65132387+dhautzin@users.noreply.github.com>
2020-05-10 14:32:04 -0500

    Add files via upload

---
+++ DroidCafeOptions/app/src/main/java/com/example/droidcafeoptions/OrderActivity.java
@@ -0,0 +1,122 @@
+package com.example.droidcafeoptions;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+/**
+ * This activity handles radio buttons for choosing a delivery method for an
+ * order, and EditText input controls.
+ */
+public class OrderActivity extends AppCompatActivity {
+
+    private static final String TAG = "OrderActivity";
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
+    /**
+     * Sets the content view to activity_order, and gets the intent and its
+     * data.
+     *
+     * @param savedInstanceState Saved instance state bundle.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        // Get the intent and its data.
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.order_textview);
+        textView.setText(message);
+
+        mDisplayDate = (TextView) findViewById(R.id.date_picker);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener(){
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view)
+            {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(OrderActivity.this, android.R.style.Theme_Black, mDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+            }
+        });
+        mDateSetListener = new DatePickerDialog.OnDateSetListener(){
+
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                month = month+1;
+                String date = "Chosen date is: "+ month + "/" + dayOfMonth + "/" + year;
+                mDisplayDate.setText(date);
+                Toast.makeText(getApplicationContext(), date, Toast.LENGTH_SHORT).show();
+
+            }
+        };
+    }
+
+    /**
+     * Checks which radio button was clicked and displays a toast message to
+     * show the choice.
+     *
+     * @param view The radio button view.
+     */
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked.
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    // Same day service
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    // Next day delivery
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    // Pick up
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                // Do nothing.
+                break;
+        }
+    }
+
+    /**
+     * Displays the actual message in a toast message.
+     *
+     * @param message Message to display.
+     */
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+}
+
